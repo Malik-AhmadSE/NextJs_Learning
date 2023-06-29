bdfd5c4f27bdd2c2a33438b608befd51fc2986bb
ahmad <ahmadali009ali008@gmail.com>
2023-06-30 01:39:20 +0500

    use Component as well as we use in React js

--- nextlearn/src/app/page.js
+++ nextlearn/src/app/page.js
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
+      <User />
     </main>
   );
+}
+
+const User=()=>{
+  return(
+  <div>
+    <h1>User Hello! Welcome </h1>
+  </div>
+  )
 }
