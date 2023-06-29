08b660d767d611c4345faa7ed382f7e0629cbc40
ahmad <ahmadali009ali008@gmail.com>
2023-06-30 02:28:27 +0500

    pass function on Event handler

--- nextlearn/src/app/page.js
+++ nextlearn/src/app/page.js
@@ -2,11 +2,13 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  
+  const name=()=>{
+    alert('Ahmad Ali')
+  }
   return (
     <main className={styles.main}>
     <h1>Events, function and state</h1>
-    <button onClick={()=>alert('Hello world its me! ')}>Click Me </button>
+    <button onClick={name}>Click Me </button>
     </main>
   );
 }
