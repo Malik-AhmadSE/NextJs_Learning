17f0dfafcb5f5796f015a58626c171a8ad636f31
ahmad <ahmadali009ali008@gmail.com>
2023-06-30 02:29:45 +0500

    pass value as a parameter in function

--- nextlearn/src/app/page.js
+++ nextlearn/src/app/page.js
@@ -2,13 +2,13 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  const name=()=>{
-    alert('Ahmad Ali')
+  const name=(name_user)=>{
+    alert(name_user)
   }
   return (
     <main className={styles.main}>
     <h1>Events, function and state</h1>
-    <button onClick={name}>Click Me </button>
+    <button onClick={()=>name('First Name is Ahmad')}>Click Me </button>
     </main>
   );
 }
