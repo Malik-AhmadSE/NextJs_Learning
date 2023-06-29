9251c0dfda708c7e191a3fa99a321820eb6b472b
ahmad <ahmadali009ali008@gmail.com>
2023-06-30 03:05:32 +0500

    component within component

--- nextlearn/src/app/page.js
+++ nextlearn/src/app/page.js
@@ -1,14 +1,22 @@
 'use client'
 import styles from "./page.module.css";
-
+import {useState} from 'react';
 export default function Home() {
-  const name=(name_user)=>{
-    alert(name_user)
+  const [Name,setname]=useState('Ahmad');
+  const name=()=>{
+   setname('Ali')
+  }
+  const Innercomp=()=>{
+    return(
+      <h1>Inner Component</h1>
+    )
   }
   return (
     <main className={styles.main}>
-    <h1>Events, function and state</h1>
-    <button onClick={()=>name('First Name is Ahmad')}>Click Me </button>
+    <h1>Events, function and state {Name}</h1>
+    <button onClick={name}>Click Me </button>
+    <Innercomp />
+    {Innercomp()}
     </main>
   );
 }
