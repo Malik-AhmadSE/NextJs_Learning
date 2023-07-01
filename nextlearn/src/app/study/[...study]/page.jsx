9a6939ebe6d5addff07b023e1fcd16673d461748
ahmad <ahmadali009ali008@gmail.com>
2023-07-01 19:52:23 +0500

    Dynamic Routing segment using parameters

--- nextlearn/src/app/study/[...study]/page.jsx
+++ nextlearn/src/app/study/[...study]/page.jsx
@@ -1,7 +1,10 @@
-export default function page() {
+export default function page({params}) {
   return (
     <div>
       <h1>Lecture 1</h1>
+      <h3>day : {params.study[0]}</h3>
+      <h3>Lecture : {params.study[1]}</h3>
+
     </div>
   )
 }
