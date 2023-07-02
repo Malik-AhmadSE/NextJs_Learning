cf9218fcca5c3cbb63c0a4fa0a3eaffc27ee9337
ahmad <ahmadali009ali008@gmail.com>
2023-07-02 08:59:21 +0500

    restrict the middleware to redirect in to login

--- nextlearn/src/middleware.jsx
+++ nextlearn/src/middleware.jsx
@@ -1,4 +1,7 @@
+import { NextResponse } from "next/server";
 
-export  function middleware() {
-    console.log('middlewares');
+export  function middleware(request) {
+    if (request.nextUrl.pathname !=='/login') {
+        return NextResponse.redirect(new URL("/login",request.url));            
+    }
 }
