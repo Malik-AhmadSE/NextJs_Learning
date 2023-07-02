2179d0a4a482112de74251114e923b02e3f01cbd
ahmad <ahmadali009ali008@gmail.com>
2023-07-02 09:04:27 +0500

    restrict middleware at single page

--- nextlearn/src/middleware.jsx
+++ nextlearn/src/middleware.jsx
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
-export  function middleware(request) {
-    if (request.nextUrl.pathname !=='/login') {
-        return NextResponse.redirect(new URL("/login",request.url));            
-    }
+export default function middleware(request) {
+    return NextResponse.redirect(new URL("/login",request.url));            
 }
+
+export const config={
+    matcher:'/about',
+};
